f51186c57a2b6ee35ce90ddc554f31b641510d29
attoyibi <muchson@alterra.id>
2023-08-02 14:04:00 +0700

    basic code react routing version 6

--- exercise4_1_routing_basic_version6/src/App.js
+++ exercise4_1_routing_basic_version6/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <h1>Contoh Dasar</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Contoh ini menunjukkan beberapa fitur inti dari React Router, termasuk{" "}
+          <code>&lt;Route&gt;</code> bersarang, <code>&lt;Outlet&gt;</code>,{" "}
+          <code>&lt;Link&gt;</code>, dan penggunaan route "*" (dikenal sebagai
+          "splat route") untuk menampilkan halaman "tidak ditemukan" ketika
+          seseorang mengakses URL yang tidak dikenali.
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Beranda</Link>
+          </li>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
